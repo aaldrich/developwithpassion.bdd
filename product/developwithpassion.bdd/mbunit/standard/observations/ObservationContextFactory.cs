3ad9f3b5771eb7fc42c80f44fd0ee162f8570657
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2009-08-23 18:45:04 -0400

    cleaned

--- product/developwithpassion.bdd/mbunit/standard/observations/ObservationContextFactory.cs
+++ product/developwithpassion.bdd/mbunit/standard/observations/ObservationContextFactory.cs
@@ -2,19 +2,14 @@ using developwithpassion.bdd.core;
 
 namespace developwithpassion.bdd.mbunit.standard.observations
 {
-    public interface ObservationContextFactory
-    {
-        ObservationContext<Contract> create_from<Contract>(ObservationContextArgs<Contract> args);
-    }
-
-    public class ObservationContextFactoryImplementation : ObservationContextFactory
+    public class ObservationContextFactoryImplementation 
     {
         public ObservationContext<Contract> create_from<Contract>(ObservationContextArgs<Contract> args)
         {
             var dependency_builder = new SystemUnderTestDependencyBuilderImplementation(args.state, args.mock_factory);
             return new ObservationContext<Contract>(
                 args.state,
-                new ObservationCommandFactoryImplementation<Contract>(args.state, new DelegateControllerImplementation(args.test)),
+                new ObservationCommandFactoryFactory().create_from(args),
                 args.mock_factory,
                 dependency_builder,
                 new SystemUnderTestFactoryImplementation(dependency_builder));
@@ -23,7 +18,7 @@ namespace developwithpassion.bdd.mbunit.standard.observations
 
     public class ObservationCommandFactoryFactory
     {
-        ObservationCommandFactory create_from<Contract>(ObservationContextArgs<Contract> args)
+        public ObservationCommandFactory create_from<Contract>(ObservationContextArgs<Contract> args)
         {
             return new ObservationCommandFactoryImplementation<Contract>(args.state, new DelegateControllerImplementation(args.test));
         }
